ff6a8a27fa228bac34bee1d134aa007d2b11074f
davejbax <davejbax@gmail.com>
2025-01-26 16:00:00 +0000

    Add persistence to color scheme setting

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,12 @@
 (function () {
 
+  // Enable color transitions now that we've established
+  document.documentElement.classList.add('color-transitions');
+
   // This script is loaded at the end of the page, so element references should work.
   document.getElementById('toggle-eye-strain')
     .addEventListener('click', () => {
-      document.documentElement.classList.toggle('color-alt');
+      setAlternativeColor();
     });
 
 })();
